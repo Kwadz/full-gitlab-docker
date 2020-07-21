a19f15babb843823d26dada34ec9d4ea9b142c97
Richard Carrée <richard.carree@gamelounge.com>
2020-07-21 11:31:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Full GitLab (with Runner) on Docker Compose
+
+1. Add `gitlab.local` in your _/etc/hosts_ file.
+
+2. Start the containers:
+
+	docker-compose up -d
+
+3. Register your first user at http://gitlab.local:8090
+
+4. Get the token to register your runner at http://gitlab.local:8090/admin/runners
+
+5. Edit _register-gitlab-runner.sh_ to replace the token value by yours.
+
+6. Register your Runner by execute the file:
+
+	./register-gitlab-runner.sh
+
+Enjoy full GitLab locally!
---
+++ register-gitlab-runner.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Get the Shared Runner registration token from:
+# http://gitlab.local:8090/admin/runners
+
+registration_token=sxRWCsZhLF94R68Eerwb
+
+docker exec -it gitlab-runner1 \
+  gitlab-runner register \
+    --non-interactive \
+    --registration-token ${registration_token} \
+    --locked=false \
+    --description docker-stable \
+    --url http://gitlab-web \
+    --executor docker \
+    --docker-image docker:stable \
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-network-mode gitlab-network
